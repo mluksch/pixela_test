d4485548965a74eeace0766bfc581640423d990a
mluk <mikauschkal@gmail.com>
2022-01-13 01:11:44 +0100

    init

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pixela_service
 
 username = "mluk"
@@ -10,5 +12,29 @@ except Exception as e:
 
 try:
     pixela_service.create_new_graph(username=username, graphname=graphname, unit="count")
+except Exception as e:
+    print(f"error: {e}")
+
+try:
+    pixela_service.create_pixel(username=username, graphname=graphname, quantity="26",
+                                date=(datetime.datetime.now() - datetime.timedelta(days=2)))
+except Exception as e:
+    print(f"error: {e}")
+
+try:
+    svg = pixela_service.get_graph_svg_url(username=username, graphname=graphname)
+    print(svg)
+except Exception as e:
+    print(f"error: {e}")
+
+try:
+    html = pixela_service.get_graph_html_url(username=username, graphname=graphname)
+    print(html)
+except Exception as e:
+    print(f"error: {e}")
+
+try:
+    pixel_info = pixela_service.get_pixel(username=username, graphname=graphname, date=datetime.datetime.now())
+    print(pixel_info)
 except Exception as e:
     print(f"error: {e}")
--- pixela_service.py
+++ pixela_service.py
@@ -24,10 +24,15 @@ def create_user(username: str):
     print(res.json())
 
 
-def create_new_graph(username: str, graphname: str, unit: str = "commit", type: str = "int", color: str = "shibafu"):
+def _get_graph_id_from_name(graphname: str):
+    graph_id = "".join([letter for letter in graphname.lower() if letter in string.ascii_lowercase])
+    return graph_id
+
+
+def create_new_graph(username: str, graphname: str, unit: str = "count", type: str = "int", color: str = "shibafu"):
     """https://docs.pixe.la/entry/post-graph"""
     url = f"{PIXELA_ENDPOINT}/users/{username}/graphs"
-    graph_id = "".join([letter for letter in graphname.lower() if letter in string.ascii_lowercase])
+    graph_id = _get_graph_id_from_name(graphname)
     with open(f"{config.USER_TOKEN_NAME}.txt") as f:
         usertoken = f.read()
     res = requests.post(url, json={
@@ -41,3 +46,60 @@ def create_new_graph(username: str, graphname: str, unit: str = "commit", type: 
     with open(f"{config.GRAPH_TOKEN_NAME}.txt") as f:
         f.write(graph_id)
     print(res.json())
+
+
+import datetime
+
+
+def create_pixel(username: str, graphname: str, quantity: str = "1", date: datetime.datetime = datetime.datetime.now()):
+    graph_id = _get_graph_id_from_name(graphname)
+    url = f"{PIXELA_ENDPOINT}/users/{username}/graphs/{graph_id}"
+    with open(f"{config.USER_TOKEN_NAME}.txt") as f:
+        usertoken = f.read()
+    res = requests.post(url, json={
+        "date": date.strftime("%Y%m%d"),
+        "quantity": quantity
+    }, headers={
+        "X-USER-TOKEN": usertoken
+    })
+    print(res.json())
+    res.raise_for_status()
+    print(res.json())
+
+
+def get_pixel(username: str, graphname: str, date: datetime.datetime):
+    with open(f"{config.USER_TOKEN_NAME}.txt") as f:
+        usertoken = f.read()
+    graphid = _get_graph_id_from_name(graphname)
+    url = f"{PIXELA_ENDPOINT}/users/{username}/graphs/{graphid}/{date.strftime('%Y%m%d')}"
+    res = requests.get(url, headers={
+        "X-USER-TOKEN": usertoken
+    })
+    res.raise_for_status()
+    return res.json()
+
+
+def get_graph_svg_url(username: str, graphname: str):
+    graphid = _get_graph_id_from_name(graphname)
+    url = f"{PIXELA_ENDPOINT}/users/{username}/graphs/{graphid}"
+    return url
+
+
+def get_graph_svg(username: str, graphname: str):
+    url = get_graph_svg_url(username=username, graphname=graphname)
+    res = requests.get(url)
+    res.raise_for_status()
+    return res.text
+
+
+def get_graph_html_url(username: str, graphname: str, mode: str = "simple"):
+    graphid = _get_graph_id_from_name(graphname)
+    url = f"{PIXELA_ENDPOINT}/users/{username}/graphs/{graphid}.html"
+    return url
+
+
+def get_graph_html(username: str, graphname: str, mode: str = "simple"):
+    url = get_graph_html_url(username=username, graphname=graphname)
+    res = requests.get(url, params={"mode": mode})
+    res.raise_for_status()
+    return res.text
